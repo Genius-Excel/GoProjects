6b5f72c3f26e659eca0b19a0b2dde2f16f2adf30
Genius-Excel <geniusexcel9@gmail.com>
2024-11-07 13:44:14 +0100

    Main package modified.

--- 0x00-hello-world/main/main.go
+++ 0x00-hello-world/main/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/Genius-Excel/GoProjects/0x00-hello-world/fizzbuzz"
 	"github.com/Genius-Excel/GoProjects/0x00-hello-world/positivenegative"
 )
 
@@ -9,5 +12,10 @@ func main() {
 	// This is the main function to call the helper function
 
 	positivenegative.Postive_Negative()
+	fmt.Println("Fizz Buzz Question")
+
+	result := fizzbuzz.Fizzbuuz(30)
+
+	fmt.Println(result)
 
 }
