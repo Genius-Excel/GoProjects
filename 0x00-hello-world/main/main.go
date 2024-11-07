3e3c02ac20e59b49ff8bced9749df069dc5e796c
Genius-Excel <geniusexcel9@gmail.com>
2024-11-07 13:23:00 +0100

    Main package.

---
+++ 0x00-hello-world/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/Genius-Excel/GoProjects/0x00-hello-world/positivenegative"
+)
+
+func main() {
+
+	// This is the main function to call the helper function
+
+	positivenegative.Postive_Negative()
+
+}
